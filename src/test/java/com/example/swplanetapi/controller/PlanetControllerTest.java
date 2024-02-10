961fc0c9ad59b409d0b3316aa27869d7402658de
Flavio Monteiro <flaviohnm@gmail.com>
2024-02-10 14:48:19 -0300

    feat: implementation of the test in the controller layer in case of success

---
+++ src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.swplanetapi.controller;
+
+import com.example.swplanetapi.service.PlanetService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.example.swplanetapi.commom.PlanetConstants.PLANET;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private PlanetService service;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        when(service.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets")
+                .content(mapper.writeValueAsString(PLANET)).contentType((MediaType.APPLICATION_JSON)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+
+}
