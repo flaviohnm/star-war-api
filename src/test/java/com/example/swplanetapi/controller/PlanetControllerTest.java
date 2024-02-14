03e2fb9326d9f9fbe5c1e490410a64815dbf491c
Flavio Monteiro <flaviohnm@gmail.com>
2024-02-13 23:19:25 -0300

    feat: implement successful Name search test case in controller

--- src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
@@ -91,4 +91,22 @@ public class PlanetControllerTest {
                 .andExpect(status().isNotFound());
     }
 
+    @Test
+    public void getPlanet_ByExistingName_ReturnsPlanet() throws Exception {
+        when(service.getByName(PLANET.getName())).thenReturn(Optional.of(PLANET));
+
+        mockMvc.perform(
+                        get("/planets/name/" + PLANET.getName()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void getPlanet_ByUnExistingName_ReturnsNotFound() throws Exception {
+        mockMvc.perform(
+                        get("/planets/name/" + PLANET.getName()))
+                .andExpect(status().isNotFound());
+    }
+
+
 }
