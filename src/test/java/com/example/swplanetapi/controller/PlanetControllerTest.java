05f48f57808038889d6bc135d0613818f5edd776
Flavio Monteiro <flaviohnm@gmail.com>
2024-02-13 22:52:05 -0300

    feat: implement successful id search test case in controller

--- src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
@@ -11,9 +11,12 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
 import static com.example.swplanetapi.commom.PlanetConstants.PLANET;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -69,6 +72,23 @@ public class PlanetControllerTest {
                                 .content(mapper.writeValueAsString(PLANET))
                                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void getPlanet_ByExistingId_ReturnsPlanet() throws Exception {
+        when(service.getById(1L)).thenReturn(Optional.of(PLANET));
+
+        mockMvc.perform(
+                        get("/planets/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+    @Test
+    public void getPlanet_ByUnExistingId_ReturnsNotFound() throws Exception {
+        mockMvc.perform(
+                        get("/planets/1"))
+                .andExpect(status().isNotFound());
     }
 
 }
