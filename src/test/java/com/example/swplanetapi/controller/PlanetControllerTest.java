a3729205346a288ecb758e5bd7bd1d2390e7b53d
Flavio Monteiro <flaviohnm@gmail.com>
2024-02-14 12:52:00 -0300

    chore: implemented tests for the remove planets function on controller

--- src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
+++ src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,9 +19,9 @@ import java.util.Optional;
 import static com.example.swplanetapi.commom.PlanetConstants.*;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -136,6 +137,23 @@ public class PlanetControllerTest {
                         get("/planets"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void removePlanet_WithExistingId_ReturnsNoContent() throws Exception {
+        mockMvc.perform(
+                        delete("/planets/1"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void removePlanet_WithUnExistingId_ReturnsNotFound() throws Exception {
+        var planetId = 1L;
+        doThrow(new EmptyResultDataAccessException(1)).when(service).removePlanet(planetId);
+
+        mockMvc.perform(
+                        delete("/planets/" + planetId))
+                .andExpect(status().isNotFound());
     }
 
 }
